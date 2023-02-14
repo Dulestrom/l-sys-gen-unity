7d4c4556c98ef5f788df00318a8042e0172dbf69
Dulestrom <91633116+Dulestrom@users.noreply.github.com>
2023-02-15 00:49:01 +0100

    Add files via upload

--- Scripts/Rules/Rule.cs
+++ Scripts/Rules/Rule.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ProceduralCity/Rule")]
 public class Rule : ScriptableObject
 {
+    // Main Class that will allow us to create the Rules
+
     public string letter;
     
     [SerializeField]
-    private string[] _results = null;
+    private string[] _results = null; // Allows us to create a list of Rules
+
+    [SerializeField]
+    private bool _randomResult = false;
 
     public string GetResults()
     {
+        if (_randomResult)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, _results.Length);
+            return _results[randomIndex];
+        }
         return _results[0];
     }
 }
